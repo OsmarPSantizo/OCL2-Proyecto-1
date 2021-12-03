b4d626ac5e9caf8b0ab4de96afa507f7f976b740
carlosngv <carlosngv99@hotmail.com>
2021-12-02 22:29:51 -0600

    Interfaz y funcionalidades implementadas

---
+++ static/js/editor.js
@@ -0,0 +1,163 @@
+// elements and global variables
+
+const tabs = document.getElementById('tabs');
+const saveButton = document.getElementById('save');
+const openInput = document.getElementById('open');
+const addButton = document.getElementById('new');
+const tabsContainer = document.getElementById('tab-container');
+const parseButton = document.getElementById('parseButton');
+
+var counter = 1;
+var currentEditor = 'editor';
+var editorList = [];
+
+
+
+
+// events
+
+document.addEventListener('DOMContentLoaded', () => {
+    let editor = document.getElementById('editor');
+
+    createEditor( editor );
+
+    tablinks = document.getElementsByClassName("tablinks");
+    //tablinks[0].className = 'active'
+
+
+
+}, false);
+
+
+addButton.addEventListener('click', () => {
+    createTab();
+});
+
+openInput.addEventListener('change', readSingleFile, false);
+
+
+
+// Functions
+
+const createTab = ( openedFile = false, contents = '' ) => {
+    counter++;
+    // adding new tab
+    let newName = `editor${counter}`;
+    let fragment = new DocumentFragment();
+    let newButton = document.createElement('button');
+    newButton.classList.add('tablinks');
+    newButton.setAttribute('onclick', `openTab(event, '${newName}div')`)
+    // newButton.onclick = openTab(event, newName + 'div');
+    newButton.innerText = `Tab ${counter}`;
+    fragment.appendChild(newButton);
+    tabs.append(fragment);
+
+    // adding new editor
+    let newDiv = document.createElement('div');
+    newDiv.setAttribute('id', newName + 'div');
+    newDiv.classList.add('tabcontent');
+
+    let newEditor = document.createElement('textarea');
+    newEditor.setAttribute('name', newName);
+    newEditor.setAttribute('id', newName);
+    newEditor.setAttribute('cols', "20");
+    newEditor.setAttribute('rows', "10");
+
+    // In case file is opened, fill the textarea.
+    if(openedFile) {
+        newEditor.innerText = contents;
+    }
+
+    newDiv.append(newEditor);
+    createEditor( newEditor );
+
+    console.log(editorList);
+
+
+    tabsContainer.appendChild(newDiv);
+
+    newDiv.style.display = 'none';
+
+    guttersCM = document.querySelectorAll('.CodeMirror-gutter');
+    guttersCM.forEach( item => {
+        item.style.width = '22px'
+    });
+}
+
+const createEditor = ( editor ) => {
+    let newEditor = CodeMirror.fromTextArea( editor, {
+        lineNumbers: true,
+        mode: "text/x-java",
+        matchBrackets: true,
+        theme: "dracula",
+        autoCloseBrackets: true,
+        autofocus: true,
+        lineWrapping: true
+    } );
+    editorList.push(newEditor);
+    currentEditor = newEditor;
+    console.log(editorList);
+}
+
+
+const openTab = (evt = event, editorName) => {
+
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(editorName).style.display = "block";
+
+    evt.currentTarget.className += " active";
+
+
+    if(editorList.length !== 1) {
+        let txtArea = editorName.replace("div", "");
+        let num = txtArea.match(/\d+/)[0]
+        num--;
+        console.log(num);
+        currentEditor = editorList[num];
+    } else {
+        currentEditor = editorList[0];
+    }
+
+    console.log(currentEditor);
+  }
+
+  // Read file
+
+function readSingleFile(e) {
+
+    var file = e.target.files[0];
+
+    if (!file) return;
+
+    var reader = new FileReader();
+
+    reader.onload = function(e) {
+        var contents = e.target.result;
+
+        console.log('contents:', contents);
+        createTab(true, contents);
+
+
+    };
+
+    reader.readAsText(file);
+
+}
+
+
+const parse = () => {
+    console.log('parsing...');
+    //let textEditor = document.getElementById(currentEditor);
+
+    console.log(currentEditor.getValue());
+}
